971962f702a316faa5330e5dda70e7b7328f01f9
Jose Ibarra <jibarra8917@gmail.com>
2021-09-24 19:04:40 -0600

    Using a linked template using the templateUrl property in the product-list component. Structural Directives: Using an ngIf to hide the table if the product(skateboard) array is empty. Using ngFor to display sample deck data. Using a component as a directive. Used interpolation to set the page title.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,4 +6,26 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = 'Skateboard List';
+  skateboards: any[] = [
+    {
+      "productId": 2,
+      "productName": "Blind - Switch Blade",
+      "productCode": "BLD-001",
+      "releaseDate": "March 18, 2021",
+      "price": 69.90,
+      "description": "7.2 wide, super sick skating deck.",
+      "starRating": 4.2,
+      "imageUrl": "assets/images/Skateboard Decks/Blind-Switch-Blade-8.5_-Skateboard-Deck--_312720-front-US.jpg"
+    },
+    {
+      "productId": 5,
+      "productName": "Blind - Reaper OG",
+      "productCode": "BLD-002",
+      "releaseDate": "March 23, 2021",
+      "price": 79.90,
+      "description": "7.2 wide, super sick skating deck.",
+      "starRating": 4.9,
+      "imageUrl": "assets/images/Skateboard Decks/Blind-OG-Reaper-Logo-8.0_-Skateboard-Deck--_299301-front-US.jpg"
+    }
+  ];
 }
