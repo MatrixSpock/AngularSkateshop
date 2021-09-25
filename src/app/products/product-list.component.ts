379f6ced7b662a9b9808ff27ba3636595b83a88c
Jose Ibarra <jibarra8917@gmail.com>
2021-09-25 08:44:33 -0600

    Lifecycle Hooks: Importing the ngOnInit lifecycle hook from Angular Core and displaying a console.log.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./products";
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Skateboard List';
   imageWidth: number = 150;
   imageMargin: number = 2;
@@ -36,6 +37,10 @@ export class ProductListComponent {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(){
+    console.log("JRI logging ngOnInit");
   }
 
 }
